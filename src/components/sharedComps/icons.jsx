c319789b74d4db7e3d1b33634889201d178c841d
Ramniwas Gupta <ramniwas.nitrr15@gmail.com>
2024-10-17 20:45:32 +0530

    minor refactored

---
+++ src/components/sharedComps/icons.jsx
@@ -0,0 +1,34 @@
+
+import HTML from './../../assets/js-techs-icons/html.png';
+import JS from './../../assets/js-techs-icons/javascript.png';
+import CSS from './../../assets/js-techs-icons/css.png';
+import SCSS from './../../assets/js-techs-icons/isass.png';
+import TS from './../../assets/js-techs-icons/typescript.png';
+import AngularJS from './../../assets/js-techs-icons/angularjs.png';
+import ReactJS from './../../assets/js-techs-icons/react-js.png';
+import Redux from './../../assets/js-techs-icons/redux.png';
+import NodeJS from './../../assets/js-techs-icons/nodejs.png';
+import ExpressJS from './../../assets/js-techs-icons/express-js.png';
+import RollUp from './../../assets/js-techs-icons/rollup-module-bundler-for-javascript.png';
+import Bootstrap from './../../assets/js-techs-icons/bootstrap.png';
+import JSON from './../../assets/js-techs-icons/json.png';
+import Svelte from './../../assets/js-techs-icons/svelte.png';
+import VueJs from './../../assets/js-techs-icons/vuejs.png';
+
+export const techIconsList = [
+    { label: 'HTML', url: HTML },
+    { label: 'JS', url: JS },
+    { label: 'CSS', url: CSS },
+    { label: 'SCSS', url: SCSS },
+    { label: 'TS', url: TS },
+    { label: 'Bootstrap', url: Bootstrap },
+    { label: 'JSON', url: JSON },
+    { label: 'AngularJS', url: AngularJS },
+    { label: 'ReactJS', url: ReactJS },
+    { label: 'Redux', url: Redux },
+    { label: 'Svelte', url: Svelte },
+    { label: 'VueJs', url: VueJs },
+    { label: 'NodeJS', url: NodeJS },
+    { label: 'RollUp', url: RollUp },
+    { label: 'ExpressJS', url: ExpressJS }
+];
