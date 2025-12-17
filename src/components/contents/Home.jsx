7788b421ec30e810a122cded80007528b9d13bac
Ramniwas Gupta <ramniwas.nitrr15@gmail.com>
2025-12-17 22:46:21 +0530

    css updated

--- src/components/contents/Home.jsx
+++ src/components/contents/Home.jsx
@@ -4,8 +4,8 @@ import Github from './../../assets/Github.png'
 import Render from './../../assets/renderwork.png'
 
 const devOpsWork = [
-    { title: 'Jira work', subTitle: 'Perform all Jira operations for my personal projects', imgUrl: JiraWork, styles: { backgroundColor: 'bisque'} },
-    { title: 'Git work',  subTitle: 'Perform all Githb actions for my personal and official projects', imgUrl: Github, styles: { color: 'white', backgroundColor: 'firebrick'} },
+    { title: 'Jira work', subTitle: 'Managing Jira operations', imgUrl: JiraWork, styles: { backgroundColor: 'bisque'} },
+    { title: 'Git work',  subTitle: 'Performing all Githb actions', imgUrl: Github, styles: { color: 'white', backgroundColor: 'firebrick'} },
     { title: 'Render work',  subTitle: 'Build, Run and Deploy apps on render, railway, firebase etc', imgUrl: Render, styles: { color: 'white', backgroundColor: 'orange'} }
 ]
 
@@ -14,22 +14,27 @@ const HomePage = () => {
         {devOpsWork.map((dow) => {
             return <Box key={dow.title} id="jira-work"
                 className={`container ${dow.classes}`}
-                height={'80vh'}
-                display="flex" flexDirection="row"
+                display="flex"
+                sx={{
+                    height: {xs: '40vh', sm: '60vh', md: '80vh' },
+                    flexDirection: { sm: 'column', md: 'row'}
+                }}
                 style={{...dow.styles}}
             >
-                <Box display={'flex'} textAlign="center" flexDirection={'column'} width={'30%'}>
-                    <Box component="h2">
+                <Box display={'flex'} textAlign="center" flexDirection={'column'} sx={{width: { xs: '100%', sm: '40%', md: '30%' }}}>
+                    <Box component="h2" mb={0}>
                         {dow.title}
                     </Box>
-                    <Box component="h6" m={0}>
-                        {dow.subTitle}
+                    <Box component="h5" textAlign={'center'} m={0}>
+                        ({dow.subTitle})
                     </Box>
                 </Box>
-                <Box flex={1}>
-                    <img src={dow.imgUrl}
+                <Box sx={{width: { xs: '100%', sm: '85%', md: '70%'}}}>
+                    <img
+                        src={dow.imgUrl}
+                        id="home_work_img"
                         alt="something important" width="auto" height='auto' className='w-100'
-                        style={{width: '100%', transform: 'rotate3d(1, 1, 1, -45deg)'}}
+                        style={{width: '96%', padding: '2%'}}
                     />
                 </Box>
             </Box>
--- src/components/sharedComps/TechIconList.jsx
+++ src/components/sharedComps/TechIconList.jsx
@@ -3,8 +3,13 @@ import Box from '@mui/material/Box';
 
 const TechIconsList = ({ iconsList, title, imgStyles }) => {
     return <>
-        <Box component="h6" m={0} pl={8.5}>{title}</Box>
-        <Box className="tech-logo-container">
+        <Box component="h6" m={0} sx={{paddingLeft: { xs: 6, md: 7.5 }}}>{title}</Box>
+        <Box className="tech-logo-container"
+            sx={{
+                padding: { xs: 2, sm: 3, md: 7.5},
+                justifyContent: { xs: 'center', md: 'flex-start'}
+            }}
+        >
             {iconsList.map(icon =>
                 <Box key={icon.label} className="icon-wrapper">
                     <img src={icon.url} alt="technology icon" style={{...imgStyles}} />
