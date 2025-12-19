c3b78dd2fb476f3ac119877e819eb4ecd004f065
Ramniwas Gupta <ramniwas.nitrr15@gmail.com>
2025-12-20 00:28:14 +0530

    gallery updated

--- src/components/contents/MyGallery.jsx
+++ src/components/contents/MyGallery.jsx
@@ -1,12 +1,17 @@
 import Box from '@mui/material/Box';
 import { Carousel } from 'react-responsive-carousel';
 import Gallery7 from './../../assets/gallery-pictures/gallery-image-000001.png'
-import Gallery6 from './../../assets/gallery-pictures/gallery-image-000002.png'
-import Gallery3 from './../../assets/gallery-pictures/gallery-image-000003.png'
-import Gallery4 from './../../assets/gallery-pictures/gallery-image-000004.png'
-import Gallery5 from './../../assets/gallery-pictures/gallery-image-000005.png'
-import Gallery1 from './../../assets/gallery-pictures/gallery-image-000006.png'
-import Gallery2 from './../../assets/gallery-pictures/gallery-image-000007.png'
+import Gallery8 from './../../assets/gallery-pictures/gallery-image-000002.png'
+import Gallery9 from './../../assets/gallery-pictures/gallery-image-000003.png'
+import Gallery10 from './../../assets/gallery-pictures/gallery-image-000004.png'
+import Gallery11 from './../../assets/gallery-pictures/gallery-image-000005.png'
+import Gallery12 from './../../assets/gallery-pictures/gallery-image-000006.png'
+import Gallery6 from './../../assets/gallery-pictures/gallery-image-000007.png'
+import Gallery5 from './../../assets/gallery-pictures/gallery-image-000008.png'
+import Gallery4 from './../../assets/gallery-pictures/gallery-image-000009.png'
+import Gallery3 from './../../assets/gallery-pictures/gallery-image-000010.png'
+import Gallery2 from './../../assets/gallery-pictures/gallery-image-000011.png'
+import Gallery1 from './../../assets/gallery-pictures/gallery-image-000012.png'
 
 const images = [
     Gallery1,
@@ -15,7 +20,12 @@ const images = [
     Gallery4,
     Gallery5,
     Gallery6,
-    Gallery7
+    Gallery7,
+    Gallery8,
+    Gallery9,
+    Gallery10,
+    Gallery11,
+    Gallery12
 ];
 
 const Others = () => {
@@ -35,7 +45,7 @@ const Others = () => {
                         <img src={src} width="240" height='300'
                             alt={`Gallery-pic-${index + 1}`}
                             className="w-[200px] md:width-[420px] h-full object-fit rounded-lg transition-transform duration-300 hover:scale-105"
-                            style={{width: index < 2 ? 'auto' : '' }}
+                            style={{width: 'auto' }}
                         />
                     </div>
                 ))}
